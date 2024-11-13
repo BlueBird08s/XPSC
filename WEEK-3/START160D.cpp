9762ddff59e4735244eede70a37bf0b14aba1a93
BlueBird08s <99961355+BlueBird08s@users.noreply.github.com>
2024-11-13 23:23:41 +0600

    Day 5 contest

---
+++ WEEK-3/START160D.cpp
@@ -0,0 +1,34 @@
+// Movie Snacks--------------->>>
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+
+    int x,y,z;
+    cin>>x>>y>>z;
+
+    int first = 2*x + 3*y;
+
+    int second = 2*z + y;
+
+    int result = min(first,second);
+
+    cout<<result<<endl;
+  
+   return 0;
+}
+
+
+
+// Too many oranges ---->>>
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+  
+   return 0;
+}
