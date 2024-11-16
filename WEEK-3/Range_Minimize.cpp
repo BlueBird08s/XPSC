1f026e578073df7c491106eac7944e39e40f1250
BlueBird08s <99961355+BlueBird08s@users.noreply.github.com>
2024-11-16 23:07:11 +0600

    Update Range_Minimize.cpp

--- WEEK-3/Range_Minimize.cpp
+++ WEEK-3/Range_Minimize.cpp
@@ -1,35 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-ios::sync_with_stdio(false);
-cin.tie(nullptr);
 
- int n,m;
- cin>>n>>m;
- vector<int> a(n), b(m);
- for(int i=0; i<n; i++)
- {
-   cin>>a[i];
- }
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
- for(int i=0; i<m; i++)
- {
-   cin>>b[i];
- }
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        
+        for (int i = 0; i < n; ++i) 
+        {
+            cin >> a[i];
+        }
+        
+        if (n <= 2) 
+        {
+            cout << 0 << endl;
+            continue;
+        }
+        
+        sort(a.begin(), a.end());
+        int res = INT_MAX;
+        
+        res = min(res, a[n-1] - a[2]);  
+        res = min(res, a[n-3] - a[0]);
+        res = min(res, a[n-2] - a[1]);
+        
+        cout << res << endl;
+    }
 
-int l=0, r=0, cnt =0;
-
- while(r<m)
- {
-   
-   if(l<n && a[l] < b[r]){
-      cnt++, l++;
-   }
-   else{
-      cout<<cnt<<" ";
-      r++;
-   }
- }
-  
-   return 0;
+    return 0;
 }
